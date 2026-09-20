f87042622f4fd3da20bcb8e4dd51ba163ff95efd
PcfSilva <pcfsilvadev@gmail.com>
2026-09-20 13:52:09 -0400

    staling the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router"
 
+import "./header.css"
+
 export function Header() {
   return (
     <header>
       <h1 className="logo">
         <Link to="/">Prime Flix</Link>
       </h1>
-      <Link className="favorites" to="/favorites">Favoritos</Link>
+      <Link className="favorites" to="/favorites">Fav<span>❤️</span>ritos</Link>
     </header>
   )
 }
